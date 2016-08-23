033a874e41016334cbff67289ad8da6d06ae730e
Dirision <nick.dirisio@mail.utoronto.ca>
2016-08-23 00:03:02 -0400

    Created header and Linked the scraper class

--- WebScraper.cpp
+++ WebScraper.cpp
@@ -1,8 +1,39 @@
 #include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include <sstream>
+//#pragma comment(lib, "cstdlib")
+#include <cstdlib>
+#include <iostream>
 #pragma comment( lib, "wininet" )
-int main()
+using namespace std;
+
+class  WebScraper
 {
 
+ string getRandomThing()
+{
 
-return 0;
 }
+
+// Will generate a url of a random page on the random section of thingaverse
+// template: http://www.thingiverse.com/explore/random-things/page:#
+ string generatePageNumb()
+{
+	std::rand();
+	// there are 43 random pages 
+	int num = rand()%44; 
+	std::stringstream ss;
+	ss << num;
+
+	string urlName = "http://www.thingiverse.com/explore/random-things/page:" + ss.str();
+
+	return urlName;
+}
+
+
+string  getIdeaNameFromUrl(){};
+
+
+
+};
---
+++ WebScraper.h
@@ -0,0 +1,21 @@
+// #include <WebScraper>
+#ifndef WEBSCRAPER_H_
+#define WEBSCRAPER_H_
+
+
+#include <cstdlib>
+#include <string>
+//std::string;
+class WebScraper
+{
+ 
+ std::string getRandomThing();
+
+ std::string generatePageNumb();
+
+ std:: string getIdeaNameFromUrl();
+
+};
+
+
+#endif
